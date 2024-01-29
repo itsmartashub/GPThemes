0eaa8d9b22f099620c9dd4cf23f9975c976e4377
itsmartashub <coderawii@gmail.com>
2024-01-29 22:11:50 +0100

    feat: introduce OLED testing style and floating btn for GPThemes options
    - Implemented OLED testing style to facilitate theme testing
    - Added a floating button for easy access to GPThemes options
    
    Changes summary:
        - Introduced OLED testing style to aid in theme testing for improved visibility and accuracy
        - Incorporated a floating button to streamline the process of changing GPT themes for enhanced user experience

--- src/js/content.js
+++ src/js/content.js
@@ -1,1 +1,23 @@
-alert('content.js')
+createAndAppendSVGStickyBtn()
+
+function createAndAppendSVGStickyBtn() {
+	const gpthFloatingBtn = document.createElement('div')
+	gpthFloatingBtn.id = 'gpthCustomizerBtn'
+	gpthFloatingBtn.className = 'gpth__svg'
+
+	gpthFloatingBtn.innerHTML = `
+        <div class="gpth__svg-icon">🎨</div>
+        <div class="gpth__options">
+            <div class="gpth__themes">
+                <h5>THEMES</h5>
+                <div class="gpth__themes-btns">
+                    <button id="light" data-gpth-theme="light">☀️</button>
+                    <button id="dark" data-gpth-theme="dark">🌙</button>
+                    <button id="oled" data-gpth-theme="black">🌖</button>
+                </div>
+            </div>
+        </div>
+    `
+
+	document.body.appendChild(gpthFloatingBtn)
+}
