5a56a48a12a7985adeb9969ceaca8f5af3fde426
itsmartashub <coderawii@gmail.com>
2024-02-11 21:32:09 +0100

    feat(zip): introduce new Node script for creating zip files from the `extension-prod` folder
    - Implemented a new Node script to automate the creation of zip files from the extension-prod folder using npm run scripts and AdmZip
    - Added rimraf to clean up existing folders and files before creating new ones
    - Added script to build all-in-once
    
    Changes summary:
        - Introduced a new Node script for automating the creation of zip files from the extension-prod folder, streamlining the deployment process.
        - Integrated rimraf to ensure proper cleanup of existing folders and files before generating new ones.

---
+++ scripts/zip.js
@@ -0,0 +1,57 @@
+const fs = require('fs/promises')
+const AdmZip = require('adm-zip')
+const { join } = require('path')
+
+const version = '2.0.1'
+const filenameInputFolder = 'extension-prod'
+const filenameOutputFolder = 'extension-zips'
+const filenamePrefix = 'gpthemes-'
+const filenameChromium = 'chromium'
+const filenameFirefox = 'firefox-mv2'
+
+async function createZipFiles() {
+	try {
+		// Check if the output directory exists
+		try {
+			await fs.access(filenameOutputFolder)
+		} catch (error) {
+			// If the directory doesn't exist, create it
+			await fs.mkdir(filenameOutputFolder, { recursive: true })
+		}
+
+		const zipChromium = new AdmZip()
+		const zipFirefox = new AdmZip()
+
+		// Add files to the zips without filtering
+		zipChromium.addLocalFolder(`${filenameInputFolder}/${filenamePrefix}${filenameChromium}`)
+		zipFirefox.addLocalFolder(`${filenameInputFolder}/${filenamePrefix}${filenameFirefox}`)
+
+		// Explicitly create zipPath strings for clarity
+		const chromiumZipPath = join(filenameOutputFolder, `${filenamePrefix}${filenameChromium}-v${version}.zip`)
+		const firefoxZipPath = join(filenameOutputFolder, `${filenamePrefix}${filenameFirefox}-v${version}.zip`)
+
+		// Delete existing zip files if they already exist
+		await Promise.all([deleteExistingFile(chromiumZipPath), deleteExistingFile(firefoxZipPath)])
+
+		// Save the zip files
+		zipChromium.writeZip(chromiumZipPath)
+		zipFirefox.writeZip(firefoxZipPath)
+
+		console.log('Zip files created successfully!')
+	} catch (error) {
+		console.error('Error creating zip files:', error)
+	}
+}
+
+async function deleteExistingFile(filePath) {
+	try {
+		// Check if the file exists before attempting to delete
+		await fs.access(filePath)
+		// If the file exists, delete it
+		await fs.unlink(filePath)
+	} catch (error) {
+		// Ignore errors if the file doesn't exist
+	}
+}
+
+createZipFiles()
