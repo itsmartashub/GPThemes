d00147daa59ee6dfb981308799fa2f435daa2645
itsmartashub <coderawii@gmail.com>
2024-03-13 13:38:33 +0100

    feat: Add `box-shadow` effect on hover for `Response 1`/`Response 2` chat bubble
    - Implemented a `box-shadow` effect on hover for the `Response 1`/`Response 2` chat bubble to enhance interactivity and visual feedback
    
    Changes summary:
    - Added a `box-shadow` effect on hover for the `Response 1`/`Response 2` chat bubble to improve interactivity and provide visual feedback.

--- scripts/zip.js
+++ scripts/zip.js
@@ -2,6 +2,7 @@ const fs = require('fs/promises')
 const AdmZip = require('adm-zip')
 const { join } = require('path')
 
+const version = '2.0.4'
 const filenameInputFolder = 'extension-prod'
 const filenameOutputFolder = 'extension-zips'
 const filenamePrefix = 'gpthemes-'
