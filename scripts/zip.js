61b7c4980072011b817b3381e15544184e24f6f5
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2024-05-19 23:11:28 +0200

    chore(version): bump to `3.0.0`
    - Bump the version number to `3.0.0` to reflect significant changes and improvements.
    
    Changes summary:
    - Updated the version number to `3.0.0` to signify substantial updates and enhancements introduced in this release.

--- scripts/zip.js
+++ scripts/zip.js
@@ -2,7 +2,7 @@ const fs = require('fs/promises')
 const AdmZip = require('adm-zip')
 const { join } = require('path')
 
-const version = '2.0.9'
+const version = '3.0.0'
 const filenameInputFolder = 'extension-prod'
 const filenameOutputFolder = 'extension-zips'
 const filenamePrefix = 'gpthemes-'
