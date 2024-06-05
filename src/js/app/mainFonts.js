99dc5ead11a5d7867a53a50179a31257d65415d4
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2024-06-05 12:01:38 +0200

    refactor(fonts): Optimize Google Fonts URL
    - Remove unused font styles from the Google Fonts URL to improve performance
    
    Changes summary:
    - Optimized the Google Fonts URL by removing unused font styles, reducing the number of requests and improving page load performance.

--- src/js/app/mainFonts.js
+++ src/js/app/mainFonts.js
@@ -24,7 +24,8 @@ const FONT_NAMES = [
 	'Outfit',
 ]
 
-const GOOGLE_FONT_WEIGHTS = `:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900`
+// const GOOGLE_FONT_WEIGHTS = `:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900`
+const GOOGLE_FONT_WEIGHTS = `:ital,wght@0,100;0,300;0,400;0,500;0,600;0,700;1,100;1,300;1,400;1,500;1,600;1,700`
 
 let onFocusValFontSize = null,
 	onFocusValLineHeight = null,
