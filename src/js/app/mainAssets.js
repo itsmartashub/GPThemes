c61375112a56aa1c276a176e31a12c676a7dc71a
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2024-06-13 16:34:35 +0200

    feat(chat-full-width): Add the full-width chat toggle logic
    - Improve the full-width chat toggle by setting the `chat-bubble-full-width` class on checked
    - Note: Saving the setting for future page loads and across different pages is not yet implemented
    
    Changes summary:
    - Improved the full-width chat toggle behavior:
      - Set the `chat-bubble-full-width` class on checked, providing a visual indication of the selected mode.
      - Note: The setting is not yet saved for future page loads or across different pages.

--- src/js/app/mainAssets.js
+++ src/js/app/mainAssets.js
@@ -11,19 +11,42 @@ let assetsHtmlCode = `
 			textSubtitle: 'Make the chat bubbles to take the full width',
 			// textSubtitle: 'Make the chat',
 		})}
-        ${renderSwitchOption({
-			inputId: 'gpth-auto-full-width',
-			isChecked: true,
-			icon: '🚗',
-			textTitle: 'Auto Chat Full Width',
-			// textSubtitle: 'Automatic a',
-			textSubtitle: 'Automatic make chat full width when the sidebar is collapsed, and vice-versa',
-		})}
     </section>
 `
 
+/* ${renderSwitchOption({
+    inputId: 'gpth-auto-full-width',
+    isChecked: true,
+    icon: '🚗',
+    textTitle: 'Auto Chat Full Width',
+    // textSubtitle: 'Automatic a',
+    textSubtitle: 'Automatic make chat full width when the sidebar is collapsed, and vice-versa',
+})} */
+
+function toggleChatFullWidth(e) {
+	const mainConversation = document.querySelector("main:has([data-testid^='conversation-turn-'])")
+
+	console.log(e.target.checked, e.target.id)
+	console.log(mainConversation)
+
+	if (!mainConversation) return
+
+	if (e.target.checked && !mainConversation.classList.contains('gpth-chat-bubble-full-width')) {
+		mainConversation?.classList.add('gpth-chat-bubble-full-width')
+	} else {
+		mainConversation?.classList.remove('gpth-chat-bubble-full-width')
+	}
+}
+
 function handleAssetsListeners() {
 	console.log('handleAssetsListeners() called')
+
+	const selectors = {
+		chatFullWidth: document.querySelector('.gpth-settings #gpth-full-width'),
+		// gpthAutoFullWidth: document.querySelector('.gpth-settings #gpth-auto-full-width'),
+	}
+
+	selectors.chatFullWidth.addEventListener('change', toggleChatFullWidth)
 }
 
 export { assetsHtmlCode, handleAssetsListeners }
