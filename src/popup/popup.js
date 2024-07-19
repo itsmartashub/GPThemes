1f427d54e0b5744a6c4220bc4e8fd3bbabe74350
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2024-07-19 23:41:03 +0200

    chore: Bump version to `4.0.0`

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -5,31 +5,33 @@ const seeFullChangelog = (version) =>
 	`<a href="https://github.com/itsmartashub/GPThemes/releases/tag/v${version}" target="_blank" rel="noopener noreferrer" class="changelog__seefullchangelog">🚀 See full release notes</a>`
 
 let htmlChangesList = `
-<h2>✨ New Features</h2>
-<ul>
-  <li><strong>Chat Full Width Feature (<a href="https://github.com/itsmartashub/GPThemes/issues/1">#1</a>)</strong>: Implemented a new feature allowing users to toggle full-width chat bubbles, enhancing the chat interface.</li>
-  <li><strong>Chat Custom Width Slider</strong>: Added range input sliders for customizing chat bubble width, providing users with more control over the chat layout.</li>
-  <li><strong>Prompt Textarea Width Slider</strong>: Implemented a custom width slider for the prompt-textarea, allowing independent width adjustment.</li>
-  <li><strong>Textarea Sync/Lock Indicator</strong>: Added a lock icon indicator on the textarea message sync switch to show when it's locked to chat widths.</li>
-  <li><strong>Responsive Width Adjustment (<a href="https://github.com/itsmartashub/GPThemes/issues/39">#39</a>)</strong>: Implemented automatic width adjustment for chat bubbles and prompt textarea based on screen size.</li>
-</ul>
 
-<h2>🐛 Bug Fixes</h2>
-<ul>
-    <li><strong>Chat Bubbles Fix</strong>: Removed outdated UI styles to fix broken chat bubbles (<a href="https://github.com/itsmartashub/GPThemes/issues/41">#41</a>).</li>
-    <li><strong>Attach Icon Visibility</strong>: Removed background color around the attach icon in the prompt field for better visibility (<a href="https://github.com/itsmartashub/GPThemes/issues/40">#40</a>).</li>
-    <li><strong>Edit State Design</strong>: Addressed issues with the edit state design for user chat bubbles after ChatGPT UI changes (<a href="https://github.com/itsmartashub/GPThemes/issues/36">#36</a>).</li>
-    <li><strong>Dialog Styling</strong>: Resolved inconsistencies in background color and sizing for small dialogs and hover effects.</li>
-    <li><strong>Mobile Header Styling</strong>: Resolved issues with the sticky header style on mobile screens.</li>
-    <li><strong>Firefox Compatibility</strong>: Fixed range sliders not being visible in Firefox browsers.</li>
-    <li><strong>Floating Button Position</strong>: Adjusted GPThemes floating button position to prevent overlap with user account image on smaller devices.</li>
-</ul>
-
-<h2>❗ BREAKING CHANGE</h2>
+<h2>💔 Breaking Change</h2>
 <ul>
     <li>Removal of Old UI Styles (<a href="https://github.com/itsmartashub/GPThemes/issues/41">#41</a>)</li>
     <li>To address issues with chat bubbles and improve overall performance, I have removed styles related to the outdated UI.</br>
     While this fix ensures smoother operation for most users, those still using the old UI version may experience some visual inconsistencies!</li>
+</ul>
+
+<h2>✨ New Features</h2>
+<ul>
+  <li><strong>Chat Full Width Feature (<a href="https://github.com/itsmartashub/GPThemes/issues/1">#1</a>)</strong>: Implemented a new feature allowing users to toggle full-width chat bubbles.</li>
+  <li><strong>Chat Custom Width Slider</strong>: Added range input sliders for customizing chat bubble width, providing users with more control over the chat layout.</li>
+  <li><strong>Prompt Field Width Slider</strong>: Implemented a custom width slider for the prompt field, allowing independent width adjustment.</li>
+  <li><strong>Prompt Field Sync/Lock Indicator</strong>: Added a lock icon indicator on the prompt message sync switch to show when it's locked to chat widths.</li>
+  <li><strong>Responsive Width Adjustment (<a href="https://github.com/itsmartashub/GPThemes/issues/39">#39</a>)</strong>: Implemented automatic width adjustment for chat bubbles and prompt field based on screen size.</li>
+  <li><strong>UI Accent Color Update</strong>: Changed the default accent color for the light theme. </li>
+</ul>
+
+<h2>🐛 Bug Fixes</h2>
+<ul>
+    <li><strong>Chat Bubbles Fix</strong>: Removed Old UI styles to fix broken chat bubbles (<a href="https://github.com/itsmartashub/GPThemes/issues/41">#41</a>).</li>
+    <li><strong>Attach Icon Visibility</strong>: Removed background color around the attach icon in the prompt field for better visibility (<a href="https://github.com/itsmartashub/GPThemes/issues/40">#40</a>).</li>
+    <li><strong>Edit State Design</strong>: Addressed issues with the edit state design for user chat bubbles after ChatGPT UI changes (<a href="https://github.com/itsmartashub/GPThemes/issues/36">#36</a>).</li>
+    <li><strong>Dialog Styling</strong>: Resolved inconsistencies in background color and sizing for small dialogs and hover effects.</li>
+    <li><strong>Mobile Header Styling</strong>: Resolved issues with the sticky header style on mobile screens.</li>
+    <li><strong>Floating Button Position</strong>: Adjusted GPThemes floating button position to prevent overlap with user account image on smaller devices.</li>
+    <li><strong>Scroll Down Button Fix</strong>: Addressed an issue with the "Scroll Down" button position on hover, ensuring it remains accessible.</li>
 </ul>
 
 
