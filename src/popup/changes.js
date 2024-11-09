16d416e7faadeefb1b4afc39babeb354bcc3fac0
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2024-11-09 02:03:41 +0100

    fix(floating-btn): Floating button overlapping DALL-E dialog image
    - Removed z-index from the floating button to prevent it from overlapping the DALL-E dialog image
    - Ensured proper layering of elements in the dialog
    
    Changes summary:
        - Fixed the floating button from covering the DALL-E dialog image.

--- src/popup/changes.js
+++ src/popup/changes.js
@@ -4,17 +4,26 @@ let issue = (hashNumber) =>
 const RELEASE_CHANGES = `
 <h3>🛠️ Improvements</h3>
 <ul>
-	<li><strong>Hint Pills: </strong>Updated the color of <code>Search</code> and <code>Picture</code> hint pills to match the theme accent</li>
-	<li><strong>Archived Chats & Share Links: </strong>Added hover background for better UX in tables</li>
+    <li><strong>Markdown & Analysis: </strong>Improve <code>Analysis</code> UI and code snippets overall</li>
+	<li><strong>Hint Pills: </strong>Update the blue color of <code>Search</code> and <code>Picture</code> hint pills to match the theme accent</li>
+	<li><strong>Archived Chats & Share Links: </strong>Add hover background for better UX in tables</li>
+	<li><strong>ChatGPT Model Menu: </strong>Add better styling for icons in sticky header ChatGPT models dropdown</li>
 </ul>
 
 <h3>🐛 Bug Fixes</h3>
 <ul>
-    <li><strong>Attach Files Icon: </strong>Fixed positioning and background of the 📎 icon in prompt fields</li>
-    <li><strong>Popover Menu: </strong>Corrected the background of the menu that appears when typing <code>/</code> in prompt field</li>
-    <li><strong>GPTs @mentions: </strong>Fix broken styling for GPTs that appears when typing <code>@</code> in prompt field</li>
-    <li><strong>Chat Bubbles: </strong>Restored proper styles and width customization for user chat bubbles</li>
-    <li><strong>Language Dropdown: </strong>Fix language dropdown accessibility and styling in <code>Settings</code></li>
+    <li><strong>Chat Bubbles: </strong>Fix user bubble styles and width customization ${issue(70)}</li>
+    <li><strong>Prompt Field Delay: </strong>Fix delayed styles application on page load ${issue(74)}</li>
+    <li><strong>Prompt Attach Files Icon: </strong>Fix positioning and background of the 📎 icon in prompt fields ${issue(
+		66
+	)}</li>
+    <li><strong>Prompt Popover Menu: </strong>Fix broken styling of the menu that appears when typing <code>/</code> in prompt field ${issue(
+		69
+	)}</li>
+    <li><strong>Prompt GPTs @mentions: </strong>Fix broken styling for GPTs that appears when typing <code>@</code> in prompt field</li>
+    <li><strong>Language Dropdown: </strong>Fix language dropdown accessibility and styling in <code>Settings</code> ${issue(
+		72
+	)}</code></li>
     <li><strong>Archived Chats & Share Links: </strong>Fix table sticky heading background color being transparent in scrollable tables</li>
 </ul>
 `
