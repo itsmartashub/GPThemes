2d0b68a54ac4c710d3cebba2f2c053a1134fe2f9
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2024-12-09 02:03:13 +0100

    fix(analysis-code): Prevent `Results` overflowing in dialogs and ensure visibility
    - Fixed `Results` overflowing in dialogs by setting `white-space: pre-wrap`
    - Ensured `Results` visibility by adding `.markdown .prose` to the selector to avoid global `.prose pre` styling
    
    Changes summary:
        - Prevented `Results` from overflowing in dialogs and ensured they are visible by adjusting the `white-space` property and scoping the CSS selector.

--- src/popup/changes.js
+++ src/popup/changes.js
@@ -4,27 +4,45 @@ let issue = (hashNumber) =>
 const RELEASE_CHANGES = `
 <h3>🛠️ Improvements</h3>
 <ul>
-    <li><strong>Markdown & Analysis: </strong>Improve <code>Analysis</code> UI and code snippets overall</li>
-	<li><strong>Hint Pills: </strong>Update the blue color of <code>Search</code> and <code>Picture</code> hint pills to match the theme accent</li>
-	<li><strong>Archived Chats & Share Links: </strong>Add hover background for better UX in tables</li>
-	<li><strong>ChatGPT Model Menu: </strong>Add better styling for icons in sticky header ChatGPT models dropdown</li>
+	<li>
+		<strong>Prompt Field Voice Icon: </strong>Enhanced <code>Advanced Voice</code> button design ${issue(80)}
+	</li>
+	<li>
+		<strong>Prompt Field Reply: </strong>Improve <code>Reply</code> ballons, and adjust edge radius ${issue(80)}
+	</li>
+	<li>
+		<strong>Voice & Feedback: </strong>Refreshed mic and close buttons, improved <code>Voice</code>layout overall and voice chat ended feedback UI ${issue(
+			79
+		)}
+	</li>
+	<li><strong>Search Sources Dialog: </strong>Update sticky category subheaders and main header <code>Links</code> to match theme for <code>/Search</code> prompts ${issue(
+		83
+	)}</li>
+	<li><strong>Chat With Sources: </strong>Improved <code>Source</code> button styling and searched links list ${issue(
+		83
+	)}</li>
+	<li><strong>Chat Bubbles Gap: </strong>Decreise spacing between chats ${issue(82)}</li>
+	<li><strong>Analysis & Code: </strong>Improved <code>Result</code> design in bubbles and dialogs</li>
+	<li>
+		<strong>Sidebar Elements: </strong>
+		Updated unread GPT message indicators and ChatGPT logo color to match theme. Make date pills more compact ${issue(80)}
+	</li>
 </ul>
 
 <h3>🐛 Bug Fixes</h3>
 <ul>
-    <li><strong>Chat Bubbles: </strong>Fix user bubble styles and width customization ${issue(70)}</li>
-    <li><strong>Prompt Field Delay: </strong>Fix delayed styles application on page load ${issue(74)}</li>
-    <li><strong>Prompt Attach Files Icon: </strong>Fix positioning and background of the 📎 icon in prompt fields ${issue(
-		66
+	<li><strong>Chat Bubbles Flashing: </strong>Fixed flash bubble on first User prompt, finally</li>
+	<li><strong>Prompt Field Misalignment: </strong>Resolved misalignment with chats caused by empty element ${issue(
+		81
 	)}</li>
-    <li><strong>Prompt Popover Menu: </strong>Fix broken styling of the menu that appears when typing <code>/</code> in prompt field ${issue(
-		69
+	<li><strong>Prompt Field Command Menus: </strong>Fixed styling issues when typing <code>/</code> or <code>@</code> in prompt ${issue(
+		80
 	)}</li>
-    <li><strong>Prompt GPTs @mentions: </strong>Fix broken styling for GPTs that appears when typing <code>@</code> in prompt field</li>
-    <li><strong>Language Dropdown: </strong>Fix language dropdown accessibility and styling in <code>Settings</code> ${issue(
-		72
-	)}</code></li>
-    <li><strong>Archived Chats & Share Links: </strong>Fix table sticky heading background color being transparent in scrollable tables</li>
+	<li><strong>Upgrade Plus Dialog: </strong>Fixed styling issues in <code>Plus</code> upgrade pricing plan card ${issue(
+		80
+	)}</li>
+	<li><strong>Analysis & Code: Fixed <code>Always show details</code> transparency with <code>Copy code</code> wrapper</li>
+	<li><strong>DALLE: </strong>Fixed styling of action buttons within chat bubble's generated image hover states</li>
 </ul>
 `
 
