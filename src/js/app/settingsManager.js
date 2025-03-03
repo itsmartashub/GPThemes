2e4524bc570d95122b9f743c43b3f26c3c58df62
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2025-03-03 19:40:59 +0100

    fix(dialog/upgrade): Make all readable in `Select your team plan` dialog for old design too

--- src/js/app/settingsManager.js
+++ src/js/app/settingsManager.js
@@ -1,15 +1,15 @@
-import { renderColorsTab, resetAllAccents } from "./mainColors.js";
-import { renderFontsTab, handleFontsListeners } from "./mainFonts.js";
-import { renderWidthsTab, handleWidthsListeners } from "./mainWidths.js";
+import { renderColorsTab, resetAllAccents } from './mainColors.js'
+import { renderFontsTab, handleFontsListeners } from './mainFonts.js'
+import { renderWidthsTab, handleWidthsListeners } from './mainWidths.js'
 
-let $settings = null;
-let $resetAllAccentsBtn = null;
+let $settings = null
+let $resetAllAccentsBtn = null
 
-const SETTINGS_OPEN_CLASS = "gpth-settings--open";
+const SETTINGS_OPEN_CLASS = 'gpth-settings--open'
 
 async function createSettings() {
-	const gpthSettings = document.createElement("div");
-	gpthSettings.className = "gpth-settings fixed flex flex-col";
+	const gpthSettings = document.createElement('div')
+	gpthSettings.className = 'gpth-settings fixed flex flex-col'
 
 	gpthSettings.innerHTML = `
     <header class="mb-5">
@@ -32,47 +32,47 @@ async function createSettings() {
         </div>
       </div>
     </main>
-  `;
+  `
 
-	document.body.appendChild(gpthSettings);
-	cacheElements(gpthSettings);
-	addListeners();
+	document.body.appendChild(gpthSettings)
+	cacheElements(gpthSettings)
+	addListeners()
 }
 
 function cacheElements(gpthSettings) {
-	$settings = gpthSettings;
-	$resetAllAccentsBtn = $settings.querySelector("#resetAllAccents");
-	$resetAllAccentsBtn.disabled = true;
+	$settings = gpthSettings
+	$resetAllAccentsBtn = $settings.querySelector('#resetAllAccents')
+	$resetAllAccentsBtn.disabled = true
 }
 function addListeners() {
-	document.getElementById("gpth-settings-close").addEventListener("click", closeSettings);
-	handleTabsSwitching();
-	handleFontsListeners();
-	handleWidthsListeners();
-	$resetAllAccentsBtn.addEventListener("click", resetAllAccents);
+	document.getElementById('gpth-settings-close').addEventListener('click', closeSettings)
+	handleTabsSwitching()
+	handleFontsListeners()
+	handleWidthsListeners()
+	$resetAllAccentsBtn.addEventListener('click', resetAllAccents)
 }
 // ___ Settings management
 function openSettings() {
-	$settings.classList.add(SETTINGS_OPEN_CLASS);
-	$settings.addEventListener("transitionend", handleSettingsOpened);
-	$resetAllAccentsBtn.disabled = false;
+	$settings.classList.add(SETTINGS_OPEN_CLASS)
+	$settings.addEventListener('transitionend', handleSettingsOpened)
+	$resetAllAccentsBtn.disabled = false
 }
 function handleSettingsOpened() {
-	document.body.addEventListener("click", handleClickOutsideSettings);
-	$settings.removeEventListener("transitionend", handleSettingsOpened);
+	document.body.addEventListener('click', handleClickOutsideSettings)
+	$settings.removeEventListener('transitionend', handleSettingsOpened)
 }
 function closeSettings() {
-	$settings.classList.remove(SETTINGS_OPEN_CLASS);
-	document.body.removeEventListener("click", handleClickOutsideSettings);
-	$resetAllAccentsBtn.disabled = true;
+	$settings.classList.remove(SETTINGS_OPEN_CLASS)
+	document.body.removeEventListener('click', handleClickOutsideSettings)
+	$resetAllAccentsBtn.disabled = true
 }
 function handleClickOutsideSettings(e) {
-	if (!$settings.contains(e.target) && e.target.id !== "gpth-open-settings") closeSettings();
+	if (!$settings.contains(e.target) && e.target.id !== 'gpth-open-settings') closeSettings()
 }
 
 function handleTabsSwitching() {
-	const tabs = document.querySelectorAll(".gpth-settings .tab-button");
-	const panes = document.querySelectorAll(".gpth-settings .tab-pane");
+	const tabs = document.querySelectorAll('.gpth-settings .tab-button')
+	const panes = document.querySelectorAll('.gpth-settings .tab-pane')
 
 	// tabs.forEach((tab, index) => {
 	// 	tab.addEventListener('click', () => {
@@ -85,20 +85,20 @@ function handleTabsSwitching() {
 	// })
 	// Cache the query results outside the event handler to avoid repeated DOM queries
 	tabs.forEach((tab, index) => {
-		tab.addEventListener("click", () => {
-			const activeTab = document.querySelector(".tab-button.active");
-			const activePane = document.querySelector(".tab-pane:not(.hidden)");
+		tab.addEventListener('click', () => {
+			const activeTab = document.querySelector('.tab-button.active')
+			const activePane = document.querySelector('.tab-pane:not(.hidden)')
 
 			// Only do DOM updates if necessary
 			if (activeTab !== tab) {
-				activeTab.classList.remove("active");
-				activePane.classList.add("hidden");
+				activeTab.classList.remove('active')
+				activePane.classList.add('hidden')
 
-				tab.classList.add("active");
-				panes[index].classList.remove("hidden");
+				tab.classList.add('active')
+				panes[index].classList.remove('hidden')
 			}
-		});
-	});
+		})
+	})
 }
 
-export { createSettings, openSettings, closeSettings, $settings };
+export { createSettings, openSettings, closeSettings, $settings }
