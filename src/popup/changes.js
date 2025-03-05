a2a4ab50f43c552267aa60ed1cb1e106332af0e5
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2025-03-05 22:52:26 +0100

    chore(version): Bump to `4.2.4`

--- src/popup/changes.js
+++ src/popup/changes.js
@@ -5,43 +5,14 @@ const RELEASE_CHANGES = `
 <h3>✨ Improvements</h3>
 <ul>
 	<li>
-		<strong>Markdown Blockquotes: </strong>Enhanced blockquotes with background, rounded edges, and thicker borders for better highlighting
+		<strong>Typing Lag: </strong>Fix laggy typing experience in prompt field for longer chats ${issue(108)}
 	</li>
 	<li>
-		<strong>Sidebar Links Animation: </strong>Added a vertical slide animation to sidebar links for a more dynamic experience
+		<strong>User Chat Edit Button: </strong>Moved back to the original top-left position to fix overlap and disappearing issues
 	</li>
-</ul>
-
-<h3>🐛 Bug Fixes</h3>
-<ul>
-    <li>
-        <strong>Markdown Code Blocks: </strong>Removed background color from syntax highlighting for improved readability ${issue(
-			105
-		)}
-    </li>
-    <li>
-        <strong>Chat Bubble Extra Spacing: </strong>Adjusted right padding in GPT chat bubbles to fix spacing inconsistencies after removing the GPT logo ${issue(
-			106
-		)}
-    </li>
 	<li>
-		<strong>Sidebar Editing Mode: </strong>Improved background appearance of sidebar links when editing chat names
+		<strong>General Improvements: </strong>Many minor styling and performance improvements.
 	</li>
-    <li>
-        <strong>Thinking Process Line: </strong>Made the vertical line in chat bubbles visible for better clarity
-    </li>
-    <li>
-        <strong>Temporary Prompt Field: </strong> Update broken background color and <code>attach</code> icon color to differentiate temp mode from the classic
-    </li>
-</ul>
-
-<h3>🚨 Known Issues</h3>
-<ul>
-	<li><strong>Conversation Scrolling Blocked by Invisible Canvas: </strong>After using the new <code>Edit in Canvas</code> button and closing Canvas, an invisible skeleton element remains in the <code>DOM</code>, blocking conversation scroll ${issue(
-		102
-	)}</li>
-	<li>Note that this behavior is consistent even when the browser extension is turned off, indicating that the issue originates from the original ChatGPT website.</li>
-	<li>That being said, I will wait for the OpenAI team to address this.</li>
 </ul>
 `
 
