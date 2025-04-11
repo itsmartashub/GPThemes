1dd580fae121d7737a6f8e25ad5cb7dba26afb69
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2025-04-11 21:31:22 +0200

    refactor(gpth): Remove the close button from customization settings as it is unnecessary
    - Remove the close button from customization settings as it is unnecessary
    - Settings already close automatically when clicking outside the panel
    
    Changes summary:
    Removed the close button from customization settings since clicking outside the panel already closes it, simplifying the interface.

--- src/js/app/settingsManager.js
+++ src/js/app/settingsManager.js
@@ -11,12 +11,14 @@ async function createSettings() {
 	const gpthSettings = document.createElement('div')
 	gpthSettings.className = 'gpth-settings fixed flex flex-col'
 
+	/* 
+	 <button class="text-token-text-tertiary hover:text-token-text-primary absolute top-4 right-4" id="gpth-settings-close">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.34315 6.34338L17.6569 17.6571M17.6569 6.34338L6.34315 17.6571" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+      </button> */
+
 	gpthSettings.innerHTML = `
     <header class="mb-5">
-      <h2 class="mt-5 text-center font-medium gpth-settings__title"><span class="font-semibold">GPThemes</span> Customization</h2>
-      <button class="text-token-text-tertiary hover:text-token-text-primary absolute top-4 right-4" id="gpth-settings-close">
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.34315 6.34338L17.6569 17.6571M17.6569 6.34338L6.34315 17.6571" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
-      </button>
+      <h2 class="text-center font-medium gpth-settings__title"><span class="font-semibold">GPThemes</span> Customization</h2>
     </header>
     <main>
       <div class="tabs">
@@ -45,7 +47,7 @@ function cacheElements(gpthSettings) {
 	$resetAllAccentsBtn.disabled = true
 }
 function addListeners() {
-	document.getElementById('gpth-settings-close').addEventListener('click', closeSettings)
+	// document.getElementById('gpth-settings-close').addEventListener('click', closeSettings)
 	handleTabsSwitching()
 	handleFontsListeners()
 	handleWidthsListeners()
