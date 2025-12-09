fe56ed54d119e1de58d48f2a179157a506d37645
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2025-12-09 20:40:31 +0100

    chore(lint): Add Biome for linting/formatting JS, HTML, CSS, YAML and Stylelint for SCSS

---
+++ stylelint.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import("stylelint").Config} */
+export default {
+	extends: ["stylelint-config-standard", "stylelint-config-standard-scss"],
+	rules: {
+		"keyframes-name-pattern": null,
+	},
+}
